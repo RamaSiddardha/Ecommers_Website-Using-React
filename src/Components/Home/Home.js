6dc238b2f025084e6a507c57f5c1d04d1e53efde
RmaSiddardha <ramasiddardha@gmail.com>
2023-04-16 19:15:18 +0530

    Making apps error proof

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,44 +1,72 @@
 import { Button, Table } from "react-bootstrap";
-import UiCard from "../UI/Card";
+// import UiCard from "../UI/Card";
 import MovieTable from "./MovieTable";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Home = () => {
-  const [movies,setMovies] = useState([])
-  const [isLoading , setLoading] =useState(false)
+  var intervelId;
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  useEffect(() => {
+    fetchHandler();
+  }, []);
   async function fetchHandler() {
-    setLoading(true)
-    const response = await fetch("https://swapi.dev/api/films");
-    const data = await response.json();
-    const result = data.results.map((movieData) => {
-      return {
-        id: movieData.episode_id,
-        title: movieData.title,
-        director: movieData.director,
-        releasedate: movieData.release_date,
+    setLoading(true);
+    setError(null);
+    try {
+      const response = await fetch("https://swapi.dev/api/films");
+      if (!response.ok) {
+        throw new Error("SomeThing Went Wrong.....Retrying.");
       }
-    });
-    setMovies(result)
-    setLoading(false)
+      if (response.ok) {
+        clearInterval(intervelId);
+      }
+      const data = await response.json();
+      const result = data.results.map((movieData) => {
+        return {
+          id: movieData.episode_id,
+          title: movieData.title,
+          director: movieData.director,
+          releasedate: movieData.release_date,
+        };
+      });
+      setMovies(result);
+    } catch (error) {
+      setError(error.message);
+      intervelId = setInterval(() => {
+        fetchHandler();
+      }, 5000);
+      setLoading(false);
+    }
   }
 
-
+  const cancleFetchHandler = () => {
+    setError();
+    setLoading(false);
+  };
 
   return (
     <div style={{ textAlign: "center" }}>
       <Button onClick={fetchHandler}>Fetech Movies</Button>
-      {!isLoading && <Table className="table-sm m-5">
-        <thead>
-          <tr>
-            <th>Realease Date</th>
-            <th>Director</th>
-            <th>Movie Name</th>
-          </tr>
-        </thead>
-       <MovieTable movies={movies}/>
-      </Table>
-      }
-      {isLoading && <p>Loading....</p>}
+      {isLoading && (
+        <Table className="table-sm m-5">
+          <thead>
+            <tr>
+              <th>Realease Date</th>
+              <th>Director</th>
+              <th>Movie Name</th>
+            </tr>
+          </thead>
+          <MovieTable movies={movies} />
+        </Table>
+      )}
+      {isLoading && !error && <p>Loading....</p>}
+      {error && (
+        <p>
+          {error} <Button onClick={cancleFetchHandler}>Cancel</Button>
+        </p>
+      )}
     </div>
   );
 };
